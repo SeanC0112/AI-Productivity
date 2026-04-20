80b91dfab56f967394260e2485fb5d8625b15cba
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2026-04-20 14:14:33 -0700

    continue cat class

--- bot.py
+++ bot.py
@@ -3,14 +3,16 @@ from PIL import ImageGrab, Image
 from datetime import datetime
 from tkinter import *
 import os
+import math
 
 class CatChatBot:
     window = Tk()
-    display1 = Label(window, image=window.image)
     state = ""
+    frame = 0
+    frame_max = 1
+    frame_min = 0
 
     def __init__(self):
-        self.window.image = PhotoImage(file="Cat/Idle/tile000.png")
         self.window.wm_attributes("-topmost", True)
         self.window.wm_attributes("-transparent", True)
         self.window.config(bg='gray')
@@ -18,16 +20,32 @@ class CatChatBot:
         self.window.update_idletasks()
         self.window.geometry("+600+300")
 
-        self.display1.grid(row=1, column=0, padx=0, pady=0)  #Display 1
-        self.display1.config( takefocus=1)
+        
 
     def update_image(self, image_path):
         self.window.image = PhotoImage(file=image_path)
-        self.display1.config(image=self.window.image)
+        display1 = Label(self.window, image=self.window.image)
+        display1.grid(row=1, column=0, padx=0, pady=0)  #Display 1
+        display1.config(image=self.window.image, takefocus=1)
+        display1.focus_set()
         self.window.update()
+        display1.destroy()
 
     def main(self):
-        pass
+        if(self.state == ""):
+            return
+        frame_current = self.frame_min + self.frame
+        image_path = f"Cat/{self.state}/tile{"0" in range(2-math.floor(frame_current))}{frame_current}.png"
+        self.update_image(image_path)
+        self.frame += 1
+        self.frame %= self.frame_max
+
+    def set_state(self, state):
+        self.state = state
+        self.frame = 0 #convert to ints
+        self.frame_max = len(os.listdir(f"Cat/{state}"))
+        self.frame_min = os.listdir(f"Cat/{state}")[0].split("tile")[1].split(".png")[0]
+        
 
     
 
@@ -75,14 +93,11 @@ def send_to_ollama(image_base64, prompt):
         print(f"❌ Error: {e}")
         sys.exit(1)
 
-i = 0
+cat = CatChatBot()
 while True:
-    window.image = PhotoImage(file=f"Cat/Idle/tile00{i}.png")
-    display1.config(image=window.image)
-    window.update()
+    cat.set_state("Idle")
+    cat.main()
     time.sleep(0.1)
-    i += 1
-    i %= 6
     # screenshot = capture_screenshot()
     # image_base64 = screenshot_to_base64(screenshot)
     # prompt = "What is on my screen? Describe in detail."
