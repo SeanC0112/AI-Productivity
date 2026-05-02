16023c33bdb8645f541c3a9ced0e0cf4952371c2
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2026-05-02 15:52:29 -0700

    found a good library for displaying the images

---
+++ testimage.py
@@ -0,0 +1,24 @@
+from PyQt5 import QtWidgets, QtCore, QtGui
+import sys
+
+app = QtWidgets.QApplication(sys.argv)
+label = QtWidgets.QLabel()
+# Remove window borders and set it to stay on top
+label.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
+# Allow the background to be transparent
+label.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+pixmap = QtGui.QPixmap("Cat/Idle/tile000.png")
+label.setPixmap(pixmap)
+label.show()
+
+timer = QtCore.QTimer()
+timer.timeout.connect(lambda: label.setPixmap(QtGui.QPixmap("Cat/Idle/tile001.png")))  # Update the image every second
+timer.start(3000)  # Change the image every 1000 milliseconds (1 second
+
+timer2 = QtCore.QTimer()
+timer2.timeout.connect(lambda: label.clear())  # Update the image every second
+timer2.start(7000)  #
+
+
+sys.exit(app.exec_())
